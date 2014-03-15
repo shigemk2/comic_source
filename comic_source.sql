177d3f840403b865ff1d8013c7472b3afd531178
shigemk2 <i.am.shige@gmail.com>
2014-03-15 20:17:23 +0900

    Fix

--- comic_source.sql
+++ comic_source.sql
@@ -1,5 +1,6 @@
 BEGIN;
-CREATE DATABASE COMIC_SOURCE;
-CREATE TABLE K_ON (id int auto_increment, charactors text, sources text, band text, title text);
-LOAD DATA INFILE "k-on.csv" INTO TABLE K_ON FIELDS TERMINATED BY ',';
+CREATE DATABASE comic_source;
+USE comic_source;
+CREATE TABLE K_ON (id int, charactors text, sources text, band text, title text);
+LOAD DATA INFILE "/PATH/TO/k-on.csv" INTO TABLE K_ON FIELDS TERMINATED BY ',';
 COMMIT;
